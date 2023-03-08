00c5f36ae7a1a50bc4166ea8752c0255efc86160
ntzyz <i@ntzyz.io>
2023-03-08 17:01:19 +0800

    support erase single thread

--- src/command-handlers/chat-erase.ts
+++ src/command-handlers/chat-erase.ts
@@ -9,26 +9,58 @@ const handler: CommandHandler = async (ctx) => {
     return
   }
 
-  const parameters = extract_parameters(ctx.message.text)
-  if (parameters[0] !== 'confirm') {
-    await ctx.reply(`Please use <code>/chat_erase confirm</code> to confirm this action.`, {
-      reply_to_message_id: ctx.message.message_id,
-      parse_mode: 'HTML',
-    })
-    return
-  }
-
   const client = get_redis_client()
 
-  const keys = await client.keys(`ntzyz-bot::chat-gpt::message_v2::${ctx.chat.id}::*`)
+  if (ctx.message.reply_to_message?.message_id) {
+    let cursor = ctx.message.reply_to_message?.message_id
+    let count = 0
 
-  for (const key of keys) {
-    await client.del(key)
+    for (;;) {
+      try {
+        const chat_history_item_text = await client.get(`ntzyz-bot::chat-gpt::message_v2::${ctx.chat.id}::${cursor}`)
+        const chat_history_item = (await JSON.parse(chat_history_item_text)) as ChatGPT.ChatHistoryItem
+
+        await client.del(`ntzyz-bot::chat-gpt::message_v2::${ctx.chat.id}::${cursor}`)
+
+        cursor = chat_history_item.reply_to_message_id
+        count += 1
+      } catch {
+        break
+      }
+    }
+
+    if (count === 0) {
+      await ctx.reply(`Error: No message to erase.`, {
+        reply_to_message_id: ctx.message.message_id,
+      })
+    } else {
+      await ctx.reply(`Erase thread finished. Total messages deleted: ${count * 2}`, {
+        reply_to_message_id: ctx.message.message_id,
+      })
+    }
+  } else {
+    const parameters = extract_parameters(ctx.message.text)
+    if (parameters[0] !== 'confirm') {
+      await ctx.reply(
+        `Warning: this operation will delete all your chat history with ChatGPT. Please use <code>/chat_erase confirm</code> to confirm this action.`,
+        {
+          reply_to_message_id: ctx.message.message_id,
+          parse_mode: 'HTML',
+        },
+      )
+      return
+    }
+
+    const keys = await client.keys(`ntzyz-bot::chat-gpt::message_v2::${ctx.chat.id}::*`)
+
+    for (const key of keys) {
+      await client.del(key)
+    }
+
+    await ctx.reply(`Erase all finished. Total messages deleted: ${keys.length * 2}`, {
+      reply_to_message_id: ctx.message.message_id,
+    })
   }
-
-  await ctx.reply(`Erase finished. Total messages deleted: ${keys.length * 2}`, {
-    reply_to_message_id: ctx.message.message_id,
-  })
 }
 
 export default handler

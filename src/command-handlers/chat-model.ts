2d1f5c57ed02ef79c12bac72387ddd844e670555
ntzyz <i@ntzyz.io>
2023-06-16 15:11:03 +0800

    update model list

--- src/command-handlers/chat-model.ts
+++ src/command-handlers/chat-model.ts
@@ -4,7 +4,16 @@ const handler: CommandHandler = async (ctx) => {
   const client = get_redis_client()
   const [value] = extract_parameters(ctx.message.text)
 
-  if (!['gpt-4', 'gpt-4-0314', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0301'].includes(value)) {
+  if (![
+    'gpt-4',
+    'gpt-4-0613',
+    'gpt-4-32k',
+    'gpt-4-32k-0613',
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-0613',
+    'gpt-3.5-turbo-16k',
+    'gpt-3.5-turbo-16k-0613'
+  ].includes(value)) {
     await ctx.reply(
       `Usage: /chat_model model_name\nFor more info, please refer to <a href="https://platform.openai.com/docs/models/model-endpoint-compatibility">Official Documentation</a>`,
       {
